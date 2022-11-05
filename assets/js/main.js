935aaaa074956ae35a773b839962eaf98239c9df
Thach Nguyen <thachnguyen281995u@example.com>
2022-11-05 08:22:02 +0700

    tieudevasanspam

--- assets/js/main.js
+++ assets/js/main.js
@@ -240,3 +240,22 @@ $(function () {
         }
     });
 });
+
+// Question 
+$(document).ready(function() {
+
+  
+    $('[data-toggle="collapse"]').click(function() {
+      $(this).toggleClass( "active" );
+      if ($(this).hasClass("active")) {
+        $(this).text("Hide");
+      } else {
+        $(this).text("Show");
+      }
+    });
+      
+      
+    // document ready  
+    });
+    
+    
