3c1a228ff1eca4b01abd0dd8e2efb52d4fbca298
Thach Nguyen <thachnguyen281995u@example.com>
2022-11-08 13:23:04 +0700

    marquee and current time

--- assets/js/main.js
+++ assets/js/main.js
@@ -240,8 +240,79 @@ $(function () {
         }
     });
 });
+// 
+function DateAndTime() {
+	var dt = new Date();
 
-    // Mutiple Menu 
+	var Hours = dt.getHours();
+	var Min = dt.getMinutes();
+	var Sec = dt.getSeconds();
+	// var MilliSec = dt.getMilliseconds();  + MilliSec + "MilliSec " (for milliseconds).
+
+	//strings
+	var days = [
+		"sunday",
+		"monday",
+		"tuesday",
+		"wednesday",
+		"thursday",
+		"friday",
+		"saturday"
+	];
+
+	//strings
+	var months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	];
+
+	// var localTime = dt.getLocaleTimeString();
+	// var localDate = dt.getLocaleDateString();
+
+	if (Min < 10) {
+		Min === "0" + Min;
+	} //displays two digits even Min less than 10
+
+	if (Sec < 10) {
+		Sec === "0" + Sec;
+	} //displays two digits even Sec less than 10
+
+	var suffix = "AM"; //cunverting 24Hours to 12Hours with AM & PM suffix
+	if (Hours >= 12) {
+		suffix = "PM";
+		Hours = Hours - 12;
+	}
+	if (Hours === 0) {
+		Hours = 12;
+	}
+
+	// document.getElementById("time").innerHTML = localTime;
+	
+	document.getElementById("time").innerHTML =
+		Hours + " : " + Min + "  " + suffix ;
+	document.getElementById("date").innerHTML =
+		days[dt.getDay()] +
+		", " +
+		dt.getDate() +
+		" " +
+		months[dt.getMonth()] +
+		" " +
+		dt.getFullYear();
+}
+
+new DateAndTime();
+setInterval("DateAndTime()", 1000);
+
     
 
       
